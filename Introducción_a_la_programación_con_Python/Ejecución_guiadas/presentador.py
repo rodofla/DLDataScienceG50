31dc2cfc3cea3f4ba6e1529e6de0ac162b7daed1
Rodof <rodfloresp@gmail.com>
2022-06-29 19:17:01 -0400

    initial commit, subiendo presentador.py

---
+++ Introducción_a_la_programación_con_Python/Ejecución_guiadas/presentador.py
@@ -0,0 +1,18 @@
+nombre = input("Ingrese su Nombre: ")
+edad = int(input("Ingrese su Edad: "))
+ocupacion = input("Ingrese su Ocupación: ")
+lugar = input("Ingrese su Lugar de trabajo: ")
+caracteristica_1 = input("Ingrese 3 características:\n1. > ")
+caracteristica_2 = input("2. > ")
+caracteristica_3 = input("3. > ")
+pasatiempos = input("Ingrese su pasatiempo: ")
+hacer = input("¿Que quieres hacer? ")
+
+
+print (f"""
+
+Mi nombre es {nombre}, tengo {edad} años y me desempeño como {ocupacion}
+en {lugar}.
+Soy una persona {caracteristica_1}, {caracteristica_2} y {caracteristica_3}.
+
+Mi pasatiempo es {pasatiempos} y me gustaría poder {hacer}.""")
