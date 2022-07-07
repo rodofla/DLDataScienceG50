46abc3c14b04a2e8b10b407d951769fe5fb6175a
Rodof <rodfloresp@gmail.com>
2022-07-07 00:28:26 -0400

    Desafío - Estructuras de Datos - 1.- Conversor de divisas

---
+++ Introducción_a_la_programación_con_Python/Desafios/conversiones.py
@@ -0,0 +1,23 @@
+from sys import argv # importa el módulo argv
+
+
+# variables que reciben los argumentos
+sol = float(argv[1])
+arg = float(argv[2])
+usd = float(argv[3])
+clp = int(argv[4])
+
+# calculando la conversión
+sol_clp =  sol * clp
+arg_clp =  arg * clp
+usd_clp =  usd * clp
+
+# imprime el resultado
+print(f"""
+Los {clp} equivalen a:
+{sol_clp:.1f} Soles
+{arg_clp:.1f} Pesos Argentinos
+{usd_clp:.1f} Dolares
+
+
+""")
