1efdfdc33a04564326855a670ee9e47b73181d19
Rodof <rodfloresp@gmail.com>
2022-07-08 00:00:32 -0400

    desafio word_count.py

---
+++ Introducción_a_la_programación_con_Python/Desafios/word_count.py
@@ -0,0 +1,31 @@
+from sys import argv
+# function who count a distinct word in a file
+lorem = argv[1]
+
+def word_count_distinct():
+    """
+    function who count a distinct word in a file
+    """
+    try:
+        with open(lorem, 'r', encoding="UTF-8") as file:
+            char = file.read()
+            return len(set(char))
+    except FileNotFoundError:
+        return 'File not found'
+    
+def word_count_distinct_2():
+    """
+    function who count a distinct word in a file
+    """
+    try:
+        with open(lorem, 'r', encoding="UTF-8") as file:
+            char = file.read()
+            return len(set(char.split(" ")))
+    except FileNotFoundError:
+        return 'File not found'
+    
+
+print(f"El número de caracteres distintos es: {word_count_distinct()}")
+print (f"El número de palabras distintas es: {word_count_distinct_2()}")
+
+
